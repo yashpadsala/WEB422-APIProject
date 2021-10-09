4cce278a2f50094301116105fcd468f3ecedabd6
yashpadsala <72058936+yashpadsala@users.noreply.github.com>
2021-10-09 18:43:08 -0400

    final

--- controllers/combineController.js
+++ controllers/combineController.js
@@ -5,6 +5,6 @@ const combineServices = require('../services/combineServices.js');
 
 router.get("/", combineServices.mainPage);
 
-router.get("*", combineServices.errorPage)
+router.get("*", combineServices.errorPage);
 
 module.exports = router
