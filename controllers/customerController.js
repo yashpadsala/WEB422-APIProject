c702c5aa2215f039b90fc61c9d6af515e08cc986
yashpadsala <72058936+yashpadsala@users.noreply.github.com>
2021-10-09 09:45:08 -0400

    second

--- controllers/customerController.js
+++ controllers/customerController.js
@@ -3,10 +3,10 @@ var router = express.Router()
 
 const customerService = require('../services/customerService.js');
 
-router.get("", customerService.getAllCustomers);
+router.get("/", customerService.getAllCustomers);
 
 router.get("/:id", customerService.getACustomer);
 
-router.post("", customerService.createACustomer);
+router.post("/", customerService.createACustomer);
 
 module.exports = router
--- controllers/productController.js
+++ controllers/productController.js
@@ -3,11 +3,11 @@ var router = express.Router()
 
 const productService = require('../services/productService.js');
 
-router.get("", productService.getAllProducts);
+router.get("/", productService.getAllProducts);
 
 router.get("/:id", productService.getAProduct);
 
-router.post("", productService.createAProduct);
+router.post("/", productService.createAProduct);
 
 router.put("/:id", productService.updateAProduct);
 
